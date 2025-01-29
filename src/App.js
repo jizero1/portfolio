1260f4f1fbecc35ce78b0a3039475c099497b6e4
jizero4101@naver.com <jizero4101@naver.com>
2025-01-29 23:57:57 +0900

    메인 화면 디자인

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import './App.css';
 import { useState, useEffect, useCallback } from 'react';
 import React from 'react';
+// import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-
+import { motion } from 'framer-motion';
 function App() {
 
   const MainContainer = () => {
     return (
-      <div>
-        <p>Front-end Developer</p>
-        <p>CHOI JIYOUNG</p>
-        <p>PortFolio</p>
+      <div className="main-container">
+        <div className="main-text">
+          <p>CHOI JIYOUNG</p>
+          <p>front-end developer</p>
+          <p>portfolio</p>
+        </div>
+        <div className="main-circle">
+          <div className="circle">
+            <div className="circle-box"></div>
+          </div>
+        </div>
+        
       </div>
     )
   }
 
   const AboutContainer = () => {
     return (
-      <div>
-
+      <div className="about-container">
+        <motion.div
+        initial={{opacity: 0, y: 100}}
+        animate={{opacity: 1, y: 0}}
+        trasition={{duration: 1}}
+        style= {{
+          background: "white",
+          color: "black",
+          padding: "20px",
+          width: "80%",
+          textAlign: "center",
+        }}
+        >ㅇㅇㅇㅇ</motion.div>
+        {/* <motion.div
+        initial={{ opacity: 0, y: 50 }} // 초기 상태
+        animate={{ opacity: 1, y: 0 }} // 애니메이션 완료 상태
+        transition={{ duration: 1 }}   // 애니메이션 지속 시간
+        style={{
+          background: "blue",
+          color: "white",
+          padding: "20px",
+          borderRadius: "10px",
+          width: "200px",
+          textAlign: "center",
+        }}
+      >
+        Framer Motion
+      </motion.div> */}
       </div>
     )
   }
